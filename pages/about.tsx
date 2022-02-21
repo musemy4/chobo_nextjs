8927244b4213663b2c14790ec289f6ea14c12756
musemy <musemy4@gmail.com>
2022-02-21 14:54:42 +0900

    fetching data added

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Layout } from '../components/layout'
+
+const API_KEY= '3c3af03532b272f9371cfae3fdb6fdb8';
+
 export default function About() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=> {
+        (async () => {
+            const data = await( 
+                await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+            ).json();
+            console.log(data);
+        })();
+    }, []);
+
     return (
         <Layout title="About | Next.js + tailwind + keycloak Example">
             <p>어바웃페이지</p>
